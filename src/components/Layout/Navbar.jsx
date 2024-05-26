2f338e73ceff443f8a9843fb5db551af68fad4e4
Kas S <sotimkul@gmail.com>
2024-05-26 12:04:43 +0500

    Color changes

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -2,17 +2,17 @@ import {Link} from "react-router-dom"
 
 function Navbar() {
     return (
-        <nav className="bg-amber-100">
+        <nav className="bg-emerald-900">
             <div className="flex justify-between py-3.5 items-center mx-auto px-5">
-                <Link to="/" className="text-4xl font-bold text-slate-600">
+                <Link to="/" className="text-4xl font-bold text-white font-mono">
                     Collection
                 </Link>
                 <ul className="flex list-none gap-5">
                     <li>
-                        <Link to="/login" className="p-4 bg-blue-400 text-white rounded-2xl font-bold">Login</Link>
+                        <Link to="/login" className="p-4 bg-white text-black rounded-2xl font-bold">Login</Link>
                     </li>
                     <li>
-                        <Link to="/register" className="p-4 bg-emerald-400 text-white rounded-2xl font-bold">Register</Link>
+                        <Link to="/register" className="p-4 bg-emerald-600 text-white rounded-2xl font-bold">Register</Link>
                     </li>
                 </ul>
             </div>
