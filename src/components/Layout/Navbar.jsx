c989855621d129ebd37bbdc53dd48dad7c60de16
Kas S <sotimkul@gmail.com>
2024-05-26 11:55:19 +0500

    First version of Navbar added

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,6 +1,21 @@
+import {Link} from "react-router-dom"
+
 function Navbar() {
     return (
-        <nav className="flex justify-between max-container">
+        <nav className="bg-amber-100">
+            <div className="flex justify-between py-3.5 items-center mx-auto px-5">
+                <Link to="/" className="text-4xl font-bold text-slate-600">
+                    Collection
+                </Link>
+                <ul className="flex list-none gap-5">
+                    <li>
+                        <Link to="/login" className="p-4 bg-blue-400 text-white rounded-2xl font-bold">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/register" className="p-4 bg-emerald-400 text-white rounded-2xl font-bold">Register</Link>
+                    </li>
+                </ul>
+            </div>
 
         </nav>
     )
