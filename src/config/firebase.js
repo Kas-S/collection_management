ac085c4fb8c81a02bf3720e353d3a2b93fe6b547
Kas S <sotimkul@gmail.com>
2024-05-27 14:04:34 +0500

    Firebase configuration

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCORgsFnFq41jizXTk3Y9KqglwxWkp-hi8",
+    authDomain: "collection-bf516.firebaseapp.com",
+    projectId: "collection-bf516",
+    storageBucket: "collection-bf516.appspot.com",
+    messagingSenderId: "1086833565818",
+    appId: "1:1086833565818:web:39eb979555b347438a5bc3",
+    measurementId: "G-YK2HXVZH2W"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFirestore(app)
+
+export { auth, db }
