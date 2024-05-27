b9de5e830ecc1dbb78444431a2b806bc0d2f7b76
Kas S <sotimkul@gmail.com>
2024-05-27 14:12:56 +0500

    User registration with email and password added

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -7,17 +7,31 @@ import {
     Button,
     Heading
 } from "@chakra-ui/react";
+import { useState } from "react"
+import { createUserWithEmailAndPassword } from "firebase/auth"
+import { auth } from "../../config/firebase.js"
 
 function Register() {
+    const [email, setEmail] = useState(""),
+          [password, setPassword] = useState("")
+
+    const register = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
     return (
         <Container>
             <form className="bg-white mt-3 p-4 rounded-md flex flex-col items-center">
                 <Heading mb="4">Register for Collection</Heading>
                 <FormControl>
                     <FormLabel fontFamily="sans-serif" fontSize="xl" fontWeight="bold">
-                        Login:
+                        Email:
                     </FormLabel>
-                    <Input type="login"/>
+                    <Input type="email" onChange={(e) => setEmail(e.target.value)} />
                     <FormHelperText>
                         Create your login
                     </FormHelperText>
@@ -26,12 +40,12 @@ function Register() {
                     <FormLabel fontFamily="sans-serif" fontSize="xl" fontWeight="bold">
                         Password:
                     </FormLabel>
-                    <Input type="password"/>
+                    <Input type="password" onChange={(e) => setPassword(e.target.value)} />
                     <FormHelperText>
                         Create your password
                     </FormHelperText>
                 </FormControl>
-                <Button>Register</Button>
+                <Button onClick={register}>Register</Button>
             </form>
         </Container>
 
