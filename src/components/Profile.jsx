d96ba06be6c0f41a24be41b110be4e07d4d41849
Kas S <sotimkul@gmail.com>
2024-05-30 18:19:10 +0500

    Redirects from /profile to /login if not authenticated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,13 @@
-
+import {useEffect, useContext} from "react"
+import {useNavigate} from "react-router-dom"
+import {UserContext} from "../userContext.js"
 
 function Profile() {
+    const user = useContext(UserContext),
+          navigate = useNavigate()
+    useEffect(() => {
+        if (!user) navigate('/login')
+    }, [user])
     return (
         <></>
     )
