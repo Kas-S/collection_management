4322119829cd39692a81fb8283ea5ccd14d2bbc7
Kas S <sotimkul@gmail.com>
2024-05-31 15:07:47 +0500

    Unnecessary code deleted

--- src/components/PublishItem.jsx
+++ src/components/PublishItem.jsx
@@ -32,40 +32,33 @@ function PublishItem() {
         const uploadTask = uploadBytesResumable(storageRef, image)
         uploadTask.on("state_changed",
             (snapshot) => {
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case 'paused':
-                    console.log('Upload is paused');
-                    break;
-                case 'running':
-                    console.log('Upload is running');
-                    break;
-            }
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
             },
             (err) => {
-            console.error("File upload Failed: ", err)
+                console.error("File upload Failed: ", err)
             },
             () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                console.log('File available at', downloadURL);
-                const dbRef = collection(fs, 'items')
-                addDoc(dbRef, {
-                    title: title,
-                    description: description,
-                    image_url: downloadURL,
-                    date: Date.now(),
-                    user_id: user.uid
-                })
-                    .then(() => {
-                        console.log('Successfully published')
-                        navigate('/')
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    console.log('File available at', downloadURL);
+                    const dbRef = collection(fs, 'items')
+                    addDoc(dbRef, {
+                        title: title,
+                        description: description,
+                        image_url: downloadURL,
+                        date: Date.now(),
+                        user_id: user.uid
                     })
-                    .catch((error) => {
-                        console.error('Error storing image in Firestore:', error)
-                    });
-            });
-        })
+                        .then(() => {
+                            console.log('Successfully published')
+                            navigate('/')
+                        })
+                        .catch((error) => {
+                            console.error('Error storing image in Firestore:', error)
+                        });
+                });
+            }
+        )
     }
 
     return (
