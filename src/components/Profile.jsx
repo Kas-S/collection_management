eb0a7aaa943d691d6322352e36a4db490b27368d
Kas S <sotimkul@gmail.com>
2024-06-01 14:14:39 +0500

    Items published by user are being displayed at his profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,9 @@
 import {useEffect, useContext, useState} from "react"
 import {useNavigate} from "react-router-dom"
 import {UserContext} from "../userContext.js"
-import {Center, Wrap, WrapItem, Card, CardBody, Image, Heading} from "@chakra-ui/react"
+import {Container, Wrap, WrapItem, Card, CardBody, Image, Heading, Text, Highlight} from "@chakra-ui/react"
 import { fs } from "../config/firebase.js"
-import {getDoc, doc} from "firebase/firestore"
+import {getDoc, doc, getDocs, collection} from "firebase/firestore"
 
 function Profile() {
     const user = useContext(UserContext),
@@ -17,27 +17,53 @@ function Profile() {
                 .then(res => setUserData(res.data()))
         }
     }, [user])
+
+    useEffect(() => {
+        if (user) {
+            let a = []
+            getDocs(collection(fs, `users/${user.uid}/items/`))
+                .then(res => {
+                    res.forEach(doc => {
+                        a.push(doc.data())
+                    })
+                })
+            setItems(a)
+        }
+
+    }, []);
+
+    console.log(items)
+
     return (
         <>
             {userData && (
-                <Center>
+                <Container>
+                    <Card display="block">
+                        <CardBody>
+                            <Image src={userData.avatar}/>
+                            <Heading>{userData.fullName}</Heading>
+                        </CardBody>
+                    </Card>
+                    <br/>
+                    <Heading>My Items</Heading>
                     <Wrap>
-                        <WrapItem>
-                            <Card>
-                                <CardBody>
-                                    <Image src={userData.avatar}/>
-                                    <Heading></Heading>
-                                </CardBody>
-                            </Card>
-                        </WrapItem>
-                        <WrapItem>
-                            <Heading>My Items</Heading>
-                            <Wrap>
 
-                            </Wrap>
-                        </WrapItem>
+                        <>
+                            {items && items.map((item, index) => (
+                                <WrapItem key={Math.random() + index}>
+                                    <Card>
+                                        <CardBody>
+                                            <Image src={item.image_url} width="sm" height="sm" alt="Item Image" bgColor="green.500"/>
+                                            <Heading>{item.title}</Heading>
+                                            <Text>{item.description}</Text>
+                                            <Highlight query="spotlight">{item.date}</Highlight>
+                                        </CardBody>
+                                    </Card>
+                                </WrapItem>
+                            ))}
+                        </>
                     </Wrap>
-                </Center>
+                </Container>
             )}
         </>
 
